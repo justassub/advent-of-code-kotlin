fea29945c6e402a934718fa952d0c48ed1ac7f4e
Justas Subatavicius <justassub@gmail.com>
2022-12-10 11:41:44 +0200

    2022 day10 PART 1 implemented

--- src/main/kotlin/advent/of/code/year2022/day10/Day10.kt
+++ src/main/kotlin/advent/of/code/year2022/day10/Day10.kt
@@ -19,7 +19,7 @@ fun solve() {
     val resultPart1 = solvePart1(signals)
 
     println(resultPart1)
-
+    solvePart2(signals)
 }
 
 fun solvePart1(signals: List<SignalAction>): Int {
@@ -30,4 +30,38 @@ fun solvePart1(signals: List<SignalAction>): Int {
         .sumOf { it * calculator.calculateScoreOfCycles(it).last().second }
 }
 
+fun solvePart2(signals: List<SignalAction>): Unit {
 
+
+    var currentCycle = 1
+    var currentValue = 1
+    val cyclesWithValues = mutableMapOf<Int, Int>()
+    cyclesWithValues[0] = currentValue
+    cyclesWithValues[currentCycle] = currentValue
+
+    for (it in signals) {
+        if (it.signalType == SignalType.NOOP) {
+            currentCycle++
+            cyclesWithValues[currentCycle] = currentValue
+        } else {
+            currentCycle++
+            cyclesWithValues[currentCycle] = currentValue
+            currentCycle++
+            currentValue += it.value
+            cyclesWithValues[currentCycle] = currentValue
+        }
+    }
+
+
+    fun printCRT(from: Int) {
+        (1..40)
+            .map { it in (cyclesWithValues.getValue(it + from)..cyclesWithValues.getValue(it + from) + 2) }
+            .map { if (it) '#' else '.' }
+            .joinToString("")
+            .let { println(it) }
+
+    }
+
+    (0..240 step 40)
+        .forEach { printCRT(it) }
+}
