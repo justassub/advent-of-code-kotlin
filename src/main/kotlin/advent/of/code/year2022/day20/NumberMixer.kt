9164de67049bec2a1380cce5a259d7c68f8a21cc
Justas Sub <justas.subatavicius@zedge.net>
2022-12-21 08:46:08 +0200

    day 21 fixed

--- src/main/kotlin/advent/of/code/year2022/day20/NumberMixer.kt
+++ src/main/kotlin/advent/of/code/year2022/day20/NumberMixer.kt
@@ -3,39 +3,24 @@ package advent.of.code.year2022.day20
 object NumberMixer {
 
     fun mixNumbers(numbers: List<Int>): List<Int> {
-        val res = numbers.toMutableList()
-        numbers.asSequence().forEach {
+        val res = numbers.mapIndexed { i, n -> n to i }.toMutableList()
+        numbers.indices.forEach {
             mixNumber(it, res)
         }
-        return res
+        return res.map { it.first }
     }
 
-    private fun mixNumber(n: Int, allNumbers: MutableList<Int>) {
+    private fun mixNumber(originalIndex: Int, allNumbers: MutableList<Pair<Int, Int>>) {
+        val currentValue = allNumbers.first { it.second == originalIndex }
+        val n = currentValue.first
         if (n == 0) {
             return
         }
-        val currentPosition = allNumbers.indexOf(n)
-        val totalSize = allNumbers.size
+        val currentPosition = allNumbers.indexOf(currentValue)
 
         allNumbers.removeAt(currentPosition)
-        val nextPosition = currentPosition + n
+        val nextPosition = (currentPosition + n).mod(allNumbers.size)
+        allNumbers.add(nextPosition, n to originalIndex)
 
-        when {
-            nextPosition < 0 -> {
-                val nextIndex = totalSize + nextPosition - 1
-                allNumbers.add(nextIndex, n)
-            }
-
-            nextPosition > totalSize -> {
-                val nextIndex = nextPosition - totalSize + 1
-                allNumbers.add(nextIndex, n)
-
-            }
-
-            nextPosition == 0 -> allNumbers.add(totalSize - 1, n)
-
-            nextPosition == totalSize - 1 -> allNumbers.add(0, n)
-            else -> allNumbers.add(nextPosition, n)
-        }
     }
 }
