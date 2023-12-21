f8ff72ac83c071aad7189bd2f026e9748ae6ccdc
Justas Sub <justas.subatavicius@zedge.net>
2023-12-21 09:23:54 +0200

    2023 day 21 part 1  implemented

--- src/main/kotlin/advent/of/code/year2023/day21/Garden.kt
+++ src/main/kotlin/advent/of/code/year2023/day21/Garden.kt
@@ -1,16 +1,9 @@
 package advent.of.code.year2023.day21
 
 import advent.of.code.util.Point
-import advent.of.code.util.findMinMaxValues
 import advent.of.code.util.findNeighbours
-import advent.of.code.year2022.day9.Position
 
 class Garden(private val gardenData: Map<Point, Char>) {
-    private val minMaxValues = gardenData.findMinMaxValues()
-    private val minX = minMaxValues.minX
-    private val maxX = minMaxValues.maxX
-    private val minY = minMaxValues.minY
-    private val maxY = minMaxValues.maxY
 
 
     private val start = gardenData.filter { it.value == 'S' }.keys
@@ -19,7 +12,9 @@ class Garden(private val gardenData: Map<Point, Char>) {
 
     fun walkSteps(steps: Int): Set<Point> {
 
-       return (1..steps)
-            .fold(start) { acc, _ -> acc.flatMap { it.findNeighbours() }.filterNot { flowers.containsKey(it) }.toSet() }
+        return (1..steps)
+            .fold(start) { acc, _ ->
+                acc.flatMap { it.findNeighbours() }.filterNot { flowers.containsKey(it) }.toSet()
+            }
     }
 }
