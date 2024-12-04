caa03d3f433a149eb0d5cf2efbe65bb6b56ea47c
Justas Sub <justas.subatavicius@zedge.net>
2024-12-04 10:43:43 +0200

    2024 day 4 part 2 implemented

--- src/main/kotlin/advent/of/code/year2024/day4/Day4.kt
+++ src/main/kotlin/advent/of/code/year2024/day4/Day4.kt
@@ -3,6 +3,7 @@ package advent.of.code.year2024.day4
 import advent.of.code.util.*
 
 const val XMAS = "XMAS"
+val MAS = "MAS".toList()
 
 fun main() {
     val input = PointBuilder.createPointsFromFile(2024, 4)
@@ -11,6 +12,12 @@ fun main() {
 
     ALLDirections.entries.sumOf { d -> xStarts.count { canCreateXMASInDirection(it.toPair(), d, input) } }
         .printResult()
+
+    input
+        .filter { it.value == 'A' }
+        .count { isXmasTree(it.toPair(), input) }
+        .printResult()
+
 }
 
 fun canCreateXMASInDirection(
@@ -27,4 +34,22 @@ fun canCreateXMASInDirection(
         else -> false
     }
 
+}
+
+fun isXmasTree(
+    entry: Pair<Point, Char>,
+    input: Map<Point, Char>
+): Boolean {
+    val oneSideLine = listOfNotNull(
+        entry.second,
+        input[entry.first.recreateByDirectionReverse(ALLDirections.UP_LEFT)],
+        input[entry.first.recreateByDirectionReverse(ALLDirections.DOWN_RIGHT)],
+    )
+
+    val secondLine = listOfNotNull(
+        entry.second,
+        input[entry.first.recreateByDirectionReverse(ALLDirections.UP_RIGHT)],
+        input[entry.first.recreateByDirectionReverse(ALLDirections.DOWN_LEFT)],
+    )
+    return oneSideLine.containsAll(MAS) && secondLine.containsAll(MAS)
 }
