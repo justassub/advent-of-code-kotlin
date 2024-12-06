338021318ea1c5a2dc2c6e748f4ec676faad04ad
Justas Sub <justas.subatavicius@zedge.net>
2024-12-06 18:41:59 +0200

    2024 day  optimise

--- src/main/kotlin/advent/of/code/year2024/day6/Day6.kt
+++ src/main/kotlin/advent/of/code/year2024/day6/Day6.kt
@@ -3,22 +3,25 @@ package advent.of.code.year2024.day6
 import advent.of.code.util.PointBuilder
 import advent.of.code.util.action.Labyrint
 import advent.of.code.util.printResult
+import kotlin.time.measureTime
 
 fun main() {
-    val points = PointBuilder.createPointsFromFile(2024, 6)
-    val obstacles = points.filter { it.value == '#' }.keys
+    measureTime {
+        val points = PointBuilder.createPointsFromFile(2024, 6)
+        val obstacles = points.filter { it.value == '#' }.keys
 
-    val player = points.filter { it.value == '^' }.firstNotNullOf { it }.key
+        val player = points.filter { it.value == '^' }.firstNotNullOf { it }.key
 
-    Labyrint(player, obstacles)
-        .findVisitedLocationsToExit()
-        .count()
-        .printResult()
+        val findVisitedLocationsToExit = Labyrint(player, obstacles)
+            .findVisitedLocationsToExit()
 
-    val emptySpaces = points.filter { it.value == '.' }.keys
+        findVisitedLocationsToExit
+            .count()
+            .printResult()
 
-    emptySpaces.count {
-        Labyrint(player, obstacles + it).isInfiniteLoop()
+        findVisitedLocationsToExit.count {
+            Labyrint(player, obstacles + it).isInfiniteLoop()
+        }.printResult()
     }.printResult()
 
 }
