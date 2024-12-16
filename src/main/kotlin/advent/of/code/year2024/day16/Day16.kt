e00353a51bf2b9c965fec5d3c6b72f6a870a7072
Justas Sub <justas.subatavicius@zedge.net>
2024-12-16 09:50:20 +0200

    2024 day 16 part 2

--- src/main/kotlin/advent/of/code/year2024/day16/Day16.kt
+++ src/main/kotlin/advent/of/code/year2024/day16/Day16.kt
@@ -15,40 +15,57 @@ fun main() {
 
     val direction = Direction.RIGHT
 
-    val visited = mutableMapOf((start to direction) to 0L)
+
+    val min = findShortestPath(
+        mutableMapOf((start to direction) to 0L),
+        goal,
+        LinkedList(listOf(MoveStep(start, direction, 0, mutableSetOf()))),
+    )
+
+    min.printResult()
+    val seats = mutableSetOf<Point>()
 
     findShortestPath(
-        visited,
+        mutableMapOf((start to direction) to 0L),
         goal,
-        LinkedList(listOf(MoveStep(start, direction, 0)))
-    ).printResult()
-    println()
+        LinkedList(listOf(MoveStep(start, direction, 0, mutableSetOf())))
+    ) {
+        if (it.points == min) {
+            seats.addAll(it.visitedPaths)
+        }
+    }
+
+
+    println(seats.size)
 }
 
 fun findShortestPath(
     visited: MutableMap<Pair<TraversablePoint<Char>, Direction>, Long>,
     goal: TraversablePoint<Char>,
     queue: LinkedList<MoveStep>,
+    aggregateResult: (MoveStep) -> Unit = {}
 ): Long {
     var minimum = Long.MAX_VALUE
 
     while (queue.isNotEmpty()) {
         val current = queue.poll()
+        current.visitedPaths.add(current.point.point)
         if (current.point == goal) {
             minimum = min(minimum, current.points)
+            aggregateResult(current)
         }
         val direction = current.direction
         val nextPoint = current.point.findNextNeighbourByDirection(direction) { it.value != '#' }
-            ?.let { MoveStep(it, direction, current.points + 1) }
+            ?.let { MoveStep(it, direction, current.points + 1, current.visitedPaths) }
 
         val rotatePositions = direction.rotate90DegreesBothSide()
             .map { current.point.findNextNeighbourByDirection(it) { n -> n.value != '#' } to it }
             .filter { it.first != null }
-            .map { MoveStep(it.first!!, it.second, current.points + 1001) }
+            .map { MoveStep(it.first!!, it.second, current.points + 1001, current.visitedPaths.toMutableSet()) }
 
         (rotatePositions + nextPoint)
             .filterNotNull()
-            .filter { it.points < visited.getOrDefault(it.point to it.direction, Long.MAX_VALUE) }
+            .filter { it.points <= visited.getOrDefault(it.point to it.direction, Long.MAX_VALUE) }
             .forEach {
                 visited[it.point to it.direction] = it.points
                 queue.add(it)
@@ -61,5 +78,5 @@ data class MoveStep(
     val point: TraversablePoint<Char>,
     val direction: Direction,
     val points: Long,
-    val visitedPaths: MutableSet<Point> = mutableSetOf()
+    val visitedPaths: MutableSet<Point>
 )
