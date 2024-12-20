125c218cf1870a5a21fef6caa6ce21582ec37dfe
Justas Sub <justas.subatavicius@zedge.net>
2024-12-20 11:48:34 +0200

    2024 day 20 part 1

--- src/main/kotlin/advent/of/code/util/TraverseMapUtil.kt
+++ src/main/kotlin/advent/of/code/util/TraverseMapUtil.kt
@@ -19,3 +19,66 @@ fun <T> aggregatePossibleWays(
             .forEach { queue.add(it to start) }
     }
 }
+
+fun <T> findShortestPath(
+    start: TraversablePoint<T>,
+    goal: (TraversablePoint<T>),
+    filterNeighbours: (Collection<TraversablePoint<T>>) -> (Collection<TraversablePoint<T>>),
+    addScore: Int = 1,
+): Int {
+    var result = Int.MAX_VALUE
+    val visited = mutableMapOf(start to 0)
+    val queue = LinkedList(listOf(start to 0))
+    while (queue.isNotEmpty()) {
+        val (current, score) = queue.poll()
+
+        if (goal == current) {
+            result = minOf(result, score)
+        }
+        filterNeighbours(current.findAllNeighbours())
+            .map { it to score + addScore }
+            .filter { (neighbour, newScore) ->
+                visited.getOrDefault(neighbour, Int.MAX_VALUE) > newScore
+            }
+            .forEach {
+                visited[it.first] = it.second
+                queue.add(it)
+            }
+    }
+    return result
+}
+
+fun findAllPaths(
+    start: MovingData,
+    finish: TraversablePoint<Char>,
+    filterNeighbours: (MovingData) -> Collection<TraversablePoint<Char>>,
+    mapFunction: (MovingData, TraversablePoint<Char>) -> MovingData,
+    additionFilters: (MovingData) -> Boolean
+): Set<List<TraversablePoint<Char>>> {
+    val allPaths = mutableSetOf<List<TraversablePoint<Char>>>()
+    val stack = ArrayDeque<Pair<MovingData, MutableList<TraversablePoint<Char>>>>()
+    stack.push(start to mutableListOf())
+
+    while (stack.isNotEmpty()) {
+        val (current, path) = stack.pop()
+        path.add(current.point)
+
+        if (current.point == finish) {
+            allPaths.add(path)
+        } else {
+            filterNeighbours(current)
+                .filter { it !in path }
+                .map { mapFunction(current, it) }
+                .filter { additionFilters(it) }
+                .forEach { stack.push(it to path.toMutableList()) }
+        }
+    }
+
+    return allPaths
+}
+
+data class MovingData(
+    val point: TraversablePoint<Char>,
+    val seconds: Int,
+    val allowsToEnter: Int
+)
---
+++ src/main/kotlin/advent/of/code/year2024/day20/Day20.kt
@@ -0,0 +1,65 @@
+package advent.of.code.year2024.day20
+
+import advent.of.code.util.*
+
+fun main() {
+    val points = PointBuilder.createPointsFromFile(2024, 20)
+        .let { PointBuilder.traversablePointsFromMapSimpleDirections(it) { c -> c } }
+    val start = points.first { it.value == 'S' }
+    val goal = points.first { it.value == 'E' }
+
+
+    val minimumSteps = findShortestPath(start, goal, { lit -> lit.filter { it.value != '#' } })
+    val mustSaveSeconds = 7000
+
+
+    findAllPathsDFS(
+        MovingData(start, 0, 1),
+        goal,
+        { if (it.allowsToEnter > 0) it.point.findAllNeighbours() else it.point.findNeighbours { n -> n.value != '#' } },
+        { point, neighbour ->
+            MovingData(
+                neighbour,
+                point.seconds + 1,
+                if (neighbour.value == '#') point.allowsToEnter - 1 else point.allowsToEnter
+            )
+        },
+        { it.seconds < minimumSteps - mustSaveSeconds }
+    ).size
+        .printResult()
+
+}
+
+//fun <T> findAllPathsWithTeleportationShorterThan(
+//    start: MovingData,
+//    goal: TraversablePoint<T>,
+//    mustBeShorterThan: Int,
+//    onGoalCallback: (MovingData) -> Unit,
+//) {
+//    val queue = LinkedList(listOf(start))
+//    val seen = mutableSetOf<Point>()
+//    while (queue.isNotEmpty()) {
+//        val current = queue.poll()
+//        if (current.seconds >= mustBeShorterThan) {
+//            continue
+//        }
+//        if (current.point == goal) {
+//            onGoalCallback(current)
+//            continue
+//        }
+//
+//        val simpleWalks = current.point.findAllNeighbours().filter { it.value != '#' }
+//            .map { MovingData(it, current.seconds + 1, current.teleports) }
+//
+//        val teleportWalks =
+//            (if (current.teleports > 0) current.point.findNeighbours { it.value == '#' } else emptyList())
+//                .map { MovingData(it, current.seconds + 1, current.teleports - 1) }
+//
+//        (simpleWalks + teleportWalks)
+//            .filter { seen.add(it.point.point) }
+//            .forEach {
+//                queue.add(it)
+//            }
+//    }
+//}
+
