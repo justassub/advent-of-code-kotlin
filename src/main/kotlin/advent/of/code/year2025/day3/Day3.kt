d5852675d143fd2943a3f0df8a6b5cb02410b3a1
justassubatavicius <justas.subatavicius@zedge.net>
2025-12-03 09:51:53 +0200

    implement 2025-3 part 2

--- src/main/kotlin/advent/of/code/year2025/day3/Day3.kt
+++ src/main/kotlin/advent/of/code/year2025/day3/Day3.kt
@@ -7,23 +7,35 @@ fun main() {
         .map { l -> l.map { it.digitToInt() } }
         .map { Bank(it) }
 
-    println(banks.sumOf { it.findMaximumJoltOfTwoNumbers() })
-
+    println(banks.sumOf { it.findMaximumJoltOfNumbers(2) })
+    println(banks.sumOf { it.findMaximumJoltOfNumbers(12) })
 
 }
 
 private data class Bank(private val jolts: List<Int>) {
 
-    fun findMaximumJoltOfTwoNumbers(): Int {
-        val firstDigitMaxNumber = jolts.take(jolts.size - 1)
+    fun findMaximumJoltOfNumbers(totalNumber: Int): Long {
+
+        var text = ""
+        var currentIndex = 0
+        for (i in totalNumber - 1 downTo 0) {
+            val nextMaxVolt = extractNextMaxVolt(currentIndex, i)
+            text += nextMaxVolt.value.toString()
+            currentIndex = nextMaxVolt.index + 1
+        }
+        return text.toLong()
+
+
+    }
+
+    private fun extractNextMaxVolt(
+        startIndex: Int,
+        numbersLeft: Int
+    ): IndexedValue<Int> {
+        return jolts
             .withIndex()
+            .take(jolts.size - numbersLeft)
+            .drop(startIndex)
             .maxBy { it.value }
-
-        val secondMaxNumber = jolts
-            .drop(firstDigitMaxNumber.index + 1)
-            .max()
-
-        return "${firstDigitMaxNumber.value}${secondMaxNumber}".toInt()
-
     }
 }
