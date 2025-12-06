84bf765547b0cf8af3ae46ce4629ec8ddff23229
justassubatavicius <justas.subatavicius@zedge.net>
2025-12-06 16:44:56 +0200

    implement 2025-6 part 1

---
+++ src/main/kotlin/advent/of/code/year2025/day6/Day6.kt
@@ -0,0 +1,32 @@
+package advent.of.code.year2025.day6
+
+import advent.of.code.ContentReader.readFileAsLines
+import advent.of.code.util.math.MathOperation
+import advent.of.code.util.math.Operator
+import advent.of.code.util.printResult
+
+
+fun main() {
+    val data = readFileAsLines(2025, 6)
+    val mathOperators = data.last()
+        .split(Regex("\\s+"))
+        .map { Operator.fromString(it) }
+
+    val numbers = data.dropLast(1)
+        .map { it.trim() }
+        .map { it.split(Regex("\\s+")) }
+        .map { it.map { n -> n.toLong() } }
+
+
+    mathOperators
+        .mapIndexed { i, operator ->
+            MathOperation(
+                numbers = numbers.map { it[i] },
+                operator = operator
+            )
+        }
+        .sumOf { it.applyOperation() }
+        .printResult()
+
+
+}
